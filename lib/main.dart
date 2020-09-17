112b6bd5ef117f85ead28cfb813c22e3b67c0e9a
spadao <spadao@users.noreply.github.com>
2020-09-16 23:45:06 -0300

    8Ball App finalized
    Added AppBar and body
    Added image
    Added button and functionatlity

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MyEightBall());
+
+class MyEightBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Center(
+            child: Text("Ask Me Anything"),
+          ),
+        ),
+        body: Ball(),
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNum = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNum.png'),
+        ),
+      ),
+    );
+  }
+}
